4dca5bc987c75bb239bf456fef365422b2494cca
Maha Alharbi <m.a.alharbi995@gmail.com>
2021-10-28 11:07:52 +0300

    done

---
+++ labrouter/src/Contactus.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contactus() {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <p>Contact Us page </p>
+    </div>
+  );
+}
+
+export default Contactus;
